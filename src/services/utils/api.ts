a199ee770a9bd084211de3db958577266ad7e214
Iran Adryan <contatoiranadryan@gmail.com>
2022-11-16 08:36:09 -0300

    fix: change base URL to api requests

--- src/services/utils/api.ts
+++ src/services/utils/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:3000',
-  baseURL: 'http://testapi.gsafra.com',
+  baseURL: 'https://test.api.gsafra.com/',
 });
 
 export default api;
